aec6201d35f961f773155f5aec8356c17bb0e3a7
Sergey Orlov <wladimirych@gmail.com>
2020-10-01 22:45:32 +0200

    initial commit of qrcode site

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
+import * as qrcode from "qrcode";
+
 import "./index.css";
 
-function main(): void {
-    document.write("Hello, World");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.querySelector("#input");
+    const canvas = document.querySelector("#qr");
+    if (canvas === null || input === null) {
+        throw new Error("Canvas or input null");
+    }
 
-main();
+    function update(): void {
+        if (canvas === null || input === null) {
+            throw new Error("Canvas is null");
+        }
+        const text = (input as HTMLInputElement).value;
+        qrcode.toCanvas(canvas, text === "" ? [] : text, () => {
+            // no-op
+        });
+    }
+
+    input.addEventListener("input", update);
+    update();
+});
